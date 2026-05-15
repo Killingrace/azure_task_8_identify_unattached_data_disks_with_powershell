c91016d0ce1c810e969fecbb5f37f8ac36cb3ed9
Dmytro <dima.shutko5@gmail.com>
2026-05-15 18:45:14 +0200

    sol1

--- task.ps1
+++ task.ps1
@@ -1,1 +1,3 @@
-# Write your code here
+#! /usr/lib64/powershell/pwsh
+
+Get-AzDisk | Where-Object {$_.ResourceGroupName -eq "mate-azure-task-5"} | Where-Object {$_.DiskState -eq "Unattached"} | ConvertTo-Json | Out-File result.json
